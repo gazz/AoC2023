7175ae505698d25f208cbd06486d71b238f546ee
Janis Dancis <gazz@digihaze.com>
2023-12-01 13:03:46 -0700

    switched to fancy_regex to support lookahead required for overlapping results

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,7 +1,8 @@
 use std::env;
 use std::fs;
-use regex::Regex;
+use fancy_regex::Regex;
 use std::str::FromStr;
+use std::collections::HashMap;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -14,21 +15,43 @@ fn main() {
     println!("Result {calibration}");
 }
 
+
+fn as_digit(digit_or_str: &str) -> u64 {
+    let number_mapping = HashMap::from([
+        ("one", 1),
+        ("two", 2),
+        ("three", 3),
+        ("four", 4),
+        ("five", 5),
+        ("six", 6),
+        ("seven", 7),
+        ("eight", 8),
+        ("nine", 9),
+    ]);
+    match number_mapping.get(digit_or_str) {
+        Some(digit) => *digit,
+        None => u64::from_str(digit_or_str).unwrap()
+    }
+}
+
 fn calibrate(input : &str) -> u64 {
 
-    let re = Regex::new(r"\d").unwrap();
+    let re = Regex::new(r"(?=(\d|one|two|three|four|five|six|seven|eight|nine))").unwrap();
 
     // split by lines
     let numbers : Vec<_> = input.lines().map(|line| { 
-        let matches: Vec<_> = re.find_iter(line.trim()).map(|m| m.as_str()).collect();
+        let matches: Vec<_> = re.captures_iter(line.trim())
+            .map(|m| m.unwrap().get(1).unwrap().as_str()).collect();
 
-        let first = matches.first().unwrap();
-        let last = matches.last().unwrap();
+        println!("Matches: {:?}", matches);
+
+        let first = as_digit(matches.first().unwrap());
+        let last = as_digit(matches.last().unwrap());
         let formatted = format!("{}{}", first, last); 
+        println!("Line: {}, First digit: {}, last digit: {}, formatted: {}",
+            line, first, last, formatted);
         let out = u64::from_str(&formatted).unwrap();
-        // let last = &matches.last();
-        println!("Line: {}, First digit: {}, last digit: {}, formatted: {}, out: {}",
-            line, first, last, formatted, out);
+        println!("out: {}", out);
         out
     }).collect();
 
@@ -41,8 +64,39 @@ fn test_calibrate() {
         "1abc2
         pqr3stu8vwx
         a1b2c3d4e5f
-        treb7uchet
-        testwithout";
+        treb7uchet";
 
     assert_eq!(calibrate(input), 142);
 }
+
+#[test]
+fn test_calibrate_words() {
+    let input = 
+        "two1nine
+        eightwothree
+        eighthree
+        sevenine
+        abcone2threexyz
+        xtwone3four
+        4nineeightseven2
+        zoneight234
+        7pqrstsixteen";
+    assert_eq!(calibrate(input), 281 + 83 + 79);
+}
+
+// #[test]
+// fn test_fancy_re() {
+
+//     let re = Regex::new(r"(?=(\d|one|two|three|four|five|six|seven|eight|nine))").unwrap();
+//     let matches : Vec<_> = re.captures_iter("eighthree")
+//         .map(|m| m.unwrap())
+//         .collect();
+
+//     println!("Matches: {:?}", matches);
+//     let strMatches : Vec<_> = matches.iter()
+//         .map(|m| m.get(1).unwrap().as_str())
+//         .collect();
+//     println!("Str Matches: {:?}", strMatches);
+
+//     assert_eq!(0, 1);
+// }
