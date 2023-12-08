8534b1fe79d611806849e30c13853f897b6fd762
Janis Dancis <gazz@digihaze.com>
2023-12-07 18:41:09 -0700

    day7 part2

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -11,6 +11,7 @@ fn main() {
         .expect("Should have been able to read the file");
     
     part1(&contents);
+    part2(&contents);
 }
 
 
@@ -18,7 +19,7 @@ fn part1(contents: &str) {
     let mut hands : Vec<(&str, usize, usize)> = contents.lines()
         .map(|l| {
             let parts: Vec<&str> = l.split(" ").collect();
-            (parts[0], usize::from_str(parts[1]).unwrap(), hand_score(parts[0]))
+            (parts[0], usize::from_str(parts[1]).unwrap(), hand_score(parts[0], false))
         })
         .collect();
     hands.sort_by_key(|h| h.2);
@@ -33,6 +34,27 @@ fn part1(contents: &str) {
         .sum::<usize>();
 
     println!("Part1 score: {score}");
+}
+
+fn part2(contents: &str) {
+    let mut hands : Vec<(&str, usize, usize)> = contents.lines()
+        .map(|l| {
+            let parts: Vec<&str> = l.split(" ").collect();
+            (parts[0], usize::from_str(parts[1]).unwrap(), hand_score(parts[0], true))
+        })
+        .collect();
+    hands.sort_by_key(|h| h.2);
+
+    // println!("Sorted hands: {:?}", hands);
+
+    let score = hands.iter().enumerate()
+        .map(|(index, (h, bid, _))| {
+            println!("Winnings of hand {}: {} * {} -> {}", h, index + 1, bid, bid * (index + 1));
+            bid * (index + 1)
+        })
+        .sum::<usize>();
+
+    println!("Part2 score: {score}");
 }
 
 #[derive(PartialEq, PartialOrd, Debug)]
@@ -51,10 +73,18 @@ fn char_pos_power(char_power: usize, position_power: usize) -> usize {
     (char_power + 1) * 13usize.pow(position_power as u32)
 }
 
-fn hand_score(hand: &str) -> usize {
+fn hand_score(hand: &str, joker: bool) -> usize {
     // come up with a unique score for the hand based on cards, 
-    let card_scores  = vec!["A","K","Q","J","T","9","8","7","6","5","4","3","2"];
-    let hand_type_score = (hand_type(hand) as usize) * 13usize.pow(6u32);
+    let card_scores  = if joker {
+        vec!["A","K","Q","T","9","8","7","6","5","4","3","2","J"]
+    } else {
+        vec!["A","K","Q","J","T","9","8","7","6","5","4","3","2"]
+    };
+    let hand_type_score = if joker {
+        (hand_type2(hand) as usize) * 13usize.pow(6u32)
+    } else {
+        (hand_type(hand) as usize) * 13usize.pow(6u32)
+    };
 
     // A, K, Q, J, T, 9, 8, 7, 6, 5, 4, 3, or 2.
     let card_pos_score = hand.chars().enumerate()
@@ -103,6 +133,50 @@ fn hand_type(hand: &str) -> HandType {
         3 => HandType::ThreeOfAKind,
         4 => HandType::FourOfAKind,
         _ => HandType::FiveOfAKind
+    }
+}
+
+fn hand_type2(hand: &str) -> HandType {
+    // sort card by chars & then count how many dupes there are
+    let mut chars: Vec<char> = hand.chars().collect();
+    chars.sort();
+    let mut char_runs = chars.iter()
+        .map(|c| { (c, 1usize) })
+        .coalesce(|(c, n), (d, m)| {
+            if c == d { Ok((c, n + m)) } else { Err(((c, n), (d, m))) }})
+        .map(|c_res| c_res.1)
+        .filter(|c| *c > 1)
+        .collect::<Vec<usize>>();
+    char_runs.sort();
+
+    let num_jokers = chars.iter().filter(|c| *c == &'J').count();
+
+    let hand_type_fn = || {
+        if char_runs.len() == 0 { return HandType::HighCard };
+        if char_runs.len() > 1 {
+            // two pair or full house
+            if char_runs[0] == 3 || char_runs[1] == 3 {
+                return HandType::FullHouse;
+            };
+            return HandType::TwoPair;
+        }
+        match char_runs[0] {
+            2 => HandType::OnePair,
+            3 => HandType::ThreeOfAKind,
+            4 => HandType::FourOfAKind,
+            _ => HandType::FiveOfAKind
+        }
+    };
+
+    match hand_type_fn() {
+        HandType::FourOfAKind if num_jokers > 0 => HandType::FiveOfAKind,
+        HandType::FullHouse if num_jokers >= 2 => HandType::FiveOfAKind,
+        HandType::ThreeOfAKind if num_jokers > 0 => HandType::FourOfAKind,
+        HandType::TwoPair if num_jokers == 1 => HandType::FullHouse,
+        HandType::TwoPair if num_jokers > 1 => HandType::FourOfAKind,
+        HandType::OnePair if num_jokers > 0 => HandType::ThreeOfAKind,
+        HandType::HighCard if num_jokers > 0 => HandType::OnePair,
+        hand => hand
     }
 }
 
